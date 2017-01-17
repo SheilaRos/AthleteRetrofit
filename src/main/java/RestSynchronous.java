a8cca21e24f716a1dc7bfe00a8008c48e20809c6
SheilaRos <sheila.ros.cheme@gmail.com>
2017-01-17 20:56:21 +0100

    falta acabar el Synchronous y el Async

--- src/main/java/RestSynchronous.java
+++ src/main/java/RestSynchronous.java
@@ -2,9 +2,12 @@ import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Path;
 
 import java.io.IOException;
 import java.util.List;
+
+import static sun.audio.AudioPlayer.player;
 
 
 public class RestSynchronous {
@@ -42,7 +45,6 @@ public class RestSynchronous {
             System.out.println("Status code: "+response.code() + " Message error: "+response.raw());
         }
 
-
         //creamos una llamada para llamar a un atleta en concreto
         Call<Athlete> callAthlete = athleteService.getAthlete(3L);
         Response<Athlete> responseAthlete = callAthlete.execute();
@@ -53,11 +55,40 @@ public class RestSynchronous {
             System.out.println("Status code: "+responseAthlete.code() + "Message error: "+responseAthlete.errorBody());
         }
 
+        //eliminar athleta
+        Call<Void> callAthleteDelete = athleteService.deleteAthlete();
+        Response<Void> responseDelete = callAthleteDelete.execute();
+        System.out.println("DELETE status code: " + responseDelete.code());
+
+        callAllAthletes = athleteService.getAllAthlete();
+        response = callAllAthletes.execute();
+
+        System.out.println("Comprobación del delete " +
+                "Status code: " + response.code() + System.lineSeparator() +
+                "GET players: " + response.body());
 
 
 
+        //Post
+        Athlete athlete = new Athlete();
+        athlete.setName("John");
 
+        Call<Athlete> callAthletes = athleteService.createAthlete(athlete);
+        Response<Athlete> responseAthlet= callAthletes.execute();
 
-    }
+        if(responseAthlet.isSuccessful()) {
+            Athlete athleteResp = responseAthlet.body();
+            System.out.println("Status code: " + responseAthlet.code() + System.lineSeparator() +
+                    "POST player: " + athleteResp);
+
+            //Put
+            athleteResp.setNacionality("MICASA");
+            callAllAthletes= athleteService.updateAthlete(athleteResp);
+            responseAthlet= callAllAthletes.execute();
+
+            System.out.println("Status code: " + responseAthlet.code() + System.lineSeparator() +
+                    "PUT player: " + responseAthlet.body());
+
+        }
 
 }
